198da3d5fdbd9dc1099b6945f377618c67da35da
Anurag Dangi <anuragrajpoot2468@gmail.com>
2025-05-26 15:08:19 +0530

    create basic app

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Home from './pages/Home.jsx'
+import { Route, Routes } from 'react-router'
+import Feed from "./pages/Feed.jsx"
+
+const App = () => {
+
+  const [isLogin, setIsLogin] = useState(false)
+
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <div>
+
+      {
+        loading ? (<div>Loading...</div>) : (<div>
+          {
+            isLogin ? (<div>
+
+              <Routes>
+
+                <Route path='/feed' element={<Feed/>}/>
+                  
+              </Routes>
+
+            </div>) : (<div>
+
+              <Routes>
+                <Route path={"/"} element={<Home/>}/>
+              </Routes>
+            </div>)
+          }
+        </div>)
+      }
+
+    </div>
+  )
+}
+
+export default App
