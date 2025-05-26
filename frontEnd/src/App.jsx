ce9ba7a6b46af791e5932b739e4a5d662e0bebab
Anurag Dangi <anuragrajpoot2468@gmail.com>
2025-05-26 16:39:42 +0530

    create home page

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Home from './pages/Home.jsx'
 import { Route, Routes } from 'react-router'
 import Feed from "./pages/Feed.jsx"
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
 
 const App = () => {
 
@@ -10,7 +12,7 @@ const App = () => {
   const [loading, setLoading] = useState(false)
 
   return (
-    <div className=''>
+    <div className='bg-blue-100 p-10 min-h-screen text-2xl'>
 
       {
         loading ? (<div>Loading...</div>) : (<div>
@@ -28,6 +30,10 @@ const App = () => {
               <Routes>
 
                 <Route path={"/"} element={<Home />} />
+
+                <Route path={"/log-in"} element={<Login />} />
+
+                <Route path={"/sign-up"} element={<Signup />} />
 
               </Routes>
             </div>)
--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
+import logo from "../assets/images/logo.png"
+import home from "../assets/images/home.png"
+import { Link } from 'react-router'
+import { FcCloseUpMode } from "react-icons/fc";
+
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-between min-h-screen'>
 
-      <div>
-        <img src='' alt=''/>
-        <span>Mind Space +</span>
+      <div className='flex items-center gap-2.5'>
+        <img src={logo} alt='logo' className='h-10' />
+        <span className='font-bold'>Mind Space +</span>
       </div>
 
-      <div></div>
-
-      <div>
-        <span>{`made with @anuragrajpoott`}</span>
+      <div className='flex'>
+        <div className='flex flex-col gap-10 items-center justify-center w-[50%]'>
+          <span className='font-bold'>Welcome to Mind Space +</span>
+          <span className='italic'>Share your journey, connect with others, and find resources to help you thrive during you extraa hours.</span>
+          <div className='flex gap-5'>
+            <Link to={"/Log-in"}><button className='p-2.5 bg-amber-100 rounded-lg'>Continue Journey</button></Link>
+            <Link to={"/sign-up"}><button className='p-2.5 bg-amber-100 rounded-lg'>Start Journey</button></Link>
+          </div>
+        </div>
+        <div className='w-[50%]'><img src={home} alt='' /></div>
       </div>
-      
+
+      <div className='flex justify-end'>
+      <span className='font-bold flex gap-2.5 items-center'>made with <FcCloseUpMode /> @anuragrajpoott</span>
+      </div>
+
     </div>
   )
 }
