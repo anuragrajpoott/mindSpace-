48de2ea9f76dfc3c1acc7786fa63b5ff1208a455
Anurag Dangi <anuragrajpoot2468@gmail.com>
2025-05-30 14:12:05 +0530

    Update apiConnector.js

--- client/src/services/apiConnector.js
+++ client/src/services/apiConnector.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://mind-space-plus.onrender.com/",
   withCredentials: true,
 });
 
